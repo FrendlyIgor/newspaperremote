c42841331246a00926fdf5f3bf1fe7cded6ccfb8
FrendlyIgor <mr.druzhin@yandex.ru>
2023-10-04 10:00:07 +0300

    for d10

---
+++ news/management/commands/deletepst.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from news import Post
+
+
+class Command(BaseCommand):
+    help = "Удаляет все новости из какой-либо категории, но только при подтверждении действия в консоли при выполнении команды"
+    requires_migrations_checks = True
+
+    def deleteposts(self, *arg, **options):
+        self.stdout.readable()
+        self.stdout.write('Вы действительнор хотите удалить все посты? yes/no')
+        answer =  input()
+
+        if answer == 'yes': # в случае подтверждения действительно удаляем все товары
+            Post.objects.value().delete()
+            self.stdout.write(self.style.SUCCESS('Посты успешно удалены!'))
+            return
+ 
+        self.stdout.write(self.style.ERROR('Отказано')) # в случае неправильного подтверждения, говорим, что в доступе отказано
